feafb20b9673ad686d6ad9548458b16a87eb570f
neco3s <neco3coffee@gmail.com>
2023-09-05 22:12:06 +0900

    :tada:

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const handleOnclick = (e: React.MouseEvent<HTMLInputElement>) => {
+    console.log(e.currentTarget.dataset.x);
+    console.log(e.currentTarget.dataset.y);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="button" readOnly value={'÷'} data-x={'/'} data-y={'data-*だからなんでもいけるよ'} onClick={handleOnclick} />
     </div>
   );
 }
